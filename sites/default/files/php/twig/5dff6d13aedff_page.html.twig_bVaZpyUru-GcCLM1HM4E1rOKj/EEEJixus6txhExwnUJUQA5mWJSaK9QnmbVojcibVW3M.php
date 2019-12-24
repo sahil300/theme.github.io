e1fe6407826a2e2cd3f0a59f2c9432a737d60dc2
sahil300 <sahil2006sheenu@gmail.com>
2019-12-24 12:05:43 +0530

    g

---
+++ sites/default/files/php/twig/5dff6d13aedff_page.html.twig_bVaZpyUru-GcCLM1HM4E1rOKj/EEEJixus6txhExwnUJUQA5mWJSaK9QnmbVojcibVW3M.php
@@ -0,0 +1,137 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/sahil/templates/page.html.twig */
+class __TwigTemplate_58cbdc8b2c0c5f15543373ebc91e45032e7f3769d5708bbd6d54b5fe25b9b963 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 22];
+        $filters = ["escape" => 11];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
+    <title>Document</title>
+</head>
+<body>
+    <header class=\"header\">
+        ";
+        // line 11
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "header", [])), "html", null, true);
+        echo "
+        <nav class=\"menu\">
+            ";
+        // line 13
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "menu", [])), "html", null, true);
+        echo "
+        </nav>
+    </header>
+
+
+    <main class=\"main\">
+        <div class=\"content\">
+            ";
+        // line 20
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+        </div>
+        ";
+        // line 22
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar", [])) {
+            // line 23
+            echo "        <div class=\"sidebar\">
+            ";
+            // line 24
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar", [])), "html", null, true);
+            echo "
+        </div>
+        ";
+        }
+        // line 27
+        echo "    </main>
+
+    <footer class=\"footer\">
+    ";
+        // line 30
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer", [])), "html", null, true);
+        echo "
+    </footer>
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/sahil/templates/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  103 => 30,  98 => 27,  92 => 24,  89 => 23,  87 => 22,  82 => 20,  72 => 13,  67 => 11,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/sahil/templates/page.html.twig", "C:\\Users\\Navyaa\\Sites\\theme\\Theme\\themes\\sahil\\templates\\page.html.twig");
+    }
+}
